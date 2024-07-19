352f71acf2319b0a159ada5773bac2576ec9182e
George <geob3d@gmail.com>
2024-07-19 09:55:41 -0400

    Update from_dataframe.py
    Updated the FromDataframe docstring

--- keepitsql/core/from_dataframe.py
+++ keepitsql/core/from_dataframe.py
@@ -4,36 +4,13 @@ from keepitsql.core.upsert import GenerateMergeStatement
 
 class FromDataframe(GenerateMergeStatement, GenerateInsert):
     def __init__(self, dataframe):
-        """Initializes a new instance of the FromDataframe class.
+        """
+        Initializes a new instance of the FromDataframe class.
 
         Parameters
         ----------
-        - dataframe: DataFrame. The dataframe contclass FromDataframe(Upsert, Insert):
-            def __init__(self, target_table, dataframe, target_schema=None):
-
-                Initializes a new instance of the FromDataframe class, which is designed to perform upsert and insert operations on a SQL table using data from a pandas DataFrame.
-
-                Inherits from:
-                Upsert -- A class that implements the logic for updating existing records or inserting new records if they do not exist.
-                Insert -- A class that implements the logic for inserting new records into a SQL table.
-
-                Parameters
-                ----------
-                dataframe : DataFrame
-                    The pandas DataFrame containing the data that needs to be upserted or inserted into the target table.
-
-
-
-                Methods
-                -------
-                The class should implement methods inherited from `Upsert` and `Insert` classes for executing SQL operations.
-                These methods will utilize `target_table`, `dataframe`, and `target_schema` as part of their operation.
-
-                Examples
-                --------
-                df_data = pandas.read_csv('path/to/data.csv')
-                sql_upserter = FromDataframe('my_table', df_data, 'public')
-                sql_upserter.upsert()
+        dataframe : DataFrame
+            The DataFrame containing the data that needs to be upserted or inserted into the target table.
         """
         super().__init__(dataframe)
 
